d473bb5bfe203642df15fc3f0fb97458b1995a2e
234801 <234801@KB37-303-C6>
2025-09-23 13:13:42 +0300

    создала список, добавила значения

--- pr2_Sattarova/Program.cs
+++ pr2_Sattarova/Program.cs
@@ -1,4 +1,13 @@
-﻿public enum Category
+﻿using System.Collections.Generic;
+
+List<Product> products = new List<Product>();
+products.Add(new Product("Мишка", 100, 25, Category.toys));
+products.Add(new Product("Картошка", 9999, 1, Category.food));
+products.Add(new Product("Кукла", 200, 0, Category.toys));
+products.Add(new Product("Iphone", 1000, 2, Category.electro));
+products.Add(new Product("Пицца", 5, 100, Category.food));
+
+public enum Category
 {
     food = 0,
     toys = 1,
@@ -9,6 +18,7 @@ public class Product
 {
     static public int ids = 0;
     public int id { get; }
+    
     public string Name { get; set; }
     public decimal price { get; set; }
     public int quantity { get; set; }
