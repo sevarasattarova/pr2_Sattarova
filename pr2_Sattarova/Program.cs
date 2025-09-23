3e5dc8c1b98e10efaad967605ba757d445a648de
234801 <234801@KB37-303-C6>
2025-09-23 13:09:18 +0300

    создала класс, экземпляр класса, категории на основе enum и функцию вывода экземпляра класса

--- pr2_Sattarova/Program.cs
+++ pr2_Sattarova/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿public enum Category
+{
+    food = 0,
+    toys = 1,
+    electro = 2
+}
+
+public class Product
+{
+    static public int ids = 0;
+    public int id { get; }
+    public string Name { get; set; }
+    public decimal price { get; set; }
+    public int quantity { get; set; }
+    public bool InStock => quantity > 0;
+    public Category Category { get; set; }
+
+    public Product(string n, decimal p, int q, Category category)
+    {
+        if (string.IsNullOrWhiteSpace(n)) throw new ArgumentException("Название товара не должно быть пустым");
+        if (p <= 0) throw new ArgumentException("Цена должна быть положительной");
+        if (q < 0) throw new ArgumentException("Количество не может быть отрицательным");
+        ids += 1;
+        id += ids;
+        Name = n.Trim();
+        price = p;
+        quantity = q;
+        Category = category;
+    }
+    public void Vivod() { Console.WriteLine($"ID: {id} | Название: {Name} | Цена: {price:F2} | Количество {quantity} В наличии: {(InStock ? "Да" : "Нет")} | Категория: {Category} "); }
+}
