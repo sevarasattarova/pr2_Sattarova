9a282ae8b56cf520cea40de2a6570c3793400bac
234801 <234801@KB37-303-C6>
2025-09-24 11:45:20 +0300

    сделали меню, сделали запись текста в втроку и последующий список без пробелов

--- pr2_Sattarova/Program.cs
+++ pr2_Sattarova/Program.cs
@@ -1,226 +1,39 @@
-﻿using System.Collections.Generic;
-
-List<Product> products = new List<Product>();
-products.Add(new Product("Мишка", 100, 25, Category.toys));
-products.Add(new Product("Картошка", 9999, 1, Category.food));
-products.Add(new Product("Кукла", 200, 0, Category.toys));
-products.Add(new Product("Iphone", 1000, 2, Category.electro));
-products.Add(new Product("Пицца", 5, 100, Category.food));
-
-while (true)
+﻿Console.WriteLine("Введите текст на русском(миниум 100 символов)");
+string text = Console.ReadLine();
+while (text.Length > 100) 
+{ 
+    Console.WriteLine("Введите текст на русском(миниум 100 символов)");
+    text = Console.ReadLine();
+}
+string[] textlist = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+while (true) 
 {
-    Console.WriteLine("\n=== Учет товаров: меню ===");
-    Console.WriteLine("1 - Показать все товары");
-    Console.WriteLine("2 - Добавить товар");
-    Console.WriteLine("3 - Удалить товар");
-    Console.WriteLine("4 - Заказать поставку (пополнить количество)");
-    Console.WriteLine("5 - Продать товар");
-    Console.WriteLine("6 - Поиск товаров (по коду, названию, категории)");
-    Console.WriteLine("0 - Выход");
-    Console.Write("Выберите команду: ");
+    Console.WriteLine("******************");
+    Console.WriteLine("МЕНЮ");
+    Console.WriteLine("1. Кол-во слов в тексте ");
+    Console.WriteLine("2. Поиск самого короткого слова ");
+    Console.WriteLine("3. Кол-во предложений в тексте ");
+    Console.WriteLine("4. Кол-во гласных и согласных в тексте ");
+    Console.WriteLine("5. Поиск самого длинного слова ");
+    Console.WriteLine("6. Статистики по частоте встречаемости каждой буквы");
+    Console.WriteLine("7. Продолжить работу с новым текстом ");
+    Console.WriteLine("8. Сохранение всей статистики в список ");
+    Console.WriteLine("9. Статистику по прошлым текстам ");
+    Console.WriteLine("******************");
+    Console.WriteLine("Выберите команду");
     string choice = Console.ReadLine().Trim();
     Console.WriteLine();
-    switch (choice)
+    switch(choice)
     {
-        case "1": VivodAll(products); break;
-        case "2": AddProduct(products); break;
-        case "3": RemoveProduct(products); break;
-        case "4": OrderSupply(products); break;
-        case "5": SellProduct(products); break;
-        case "6": SearchProducts(products); break;
-        case "0": return;
-        default: Console.WriteLine("Неверная команда. Попробуйте снова."); break;
+        case "1": KolvoSlov(); break;
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+        default: Console.WriteLine("Невереная команда, попробуйте другую");
     }
-}
-
-static void VivodAll(List<Product> products)
-{
-    if (!products.Any()) { Console.WriteLine("Список товаров пуст."); return; }
-    foreach (var p in products) p.Vivod();
-}
-
-static void AddProduct(List<Product> products)
-{
-    Console.Write("Введите название товара: ");
-    string name = Console.ReadLine();
-    Console.Write("Введите цену товара: ");
-    decimal price = Convert.ToDecimal(Console.ReadLine());
-    Console.Write("Введите количество товара: ");
-    int qty = Convert.ToInt32(Console.ReadLine());
-    Console.Write("Введите категорию");
-    Console.WriteLine("Доступные категории:");
-    foreach (var val in Enum.GetValues(typeof(Category)))
-        Console.WriteLine($"{(int)val} - {val}");
-    int category = Convert.ToInt32(Console.ReadLine());
-    Product prod = new Product(name, price, qty, (Category)category);
-    products.Add(prod);
-    Console.WriteLine("Товар добавлен:");
-    prod.Vivod();
-}
-
-static void RemoveProduct(List<Product> products)
-{
-    Console.WriteLine("Введите id товара для удаления");
-    int input = Convert.ToInt32(Console.ReadLine());
-
-    foreach (Product t in products.ToList())
-    {
-        if (t.id == input)
-        {
-            products.Remove(t);
-        }
-
-    }
-}
-
-
-static void OrderSupply(List<Product> products)
-{
-    bool flag = true;
-    Console.WriteLine("Напишите id уже добавленного товар,на который вы хотите оформить поставку");
-    int temp = Convert.ToInt32(Console.ReadLine());
-    foreach (Product t in products)
-    {
-        if (temp == t.id)
-        {
-            Console.WriteLine("напиишите колличество товара который хотите заказать");
-            int temp1 = Convert.ToInt32(Console.ReadLine());
-            t.quantity += temp1;
-            flag = false;
-        }
-    }
-    if (flag)
-    {
-        Console.WriteLine("вашего товара не нашлось в списке(");
-    }
-}
-
-static void SellProduct(List<Product> products)
-{
-    bool flag = true;
-    Console.WriteLine("Напишите уже добавленный товар,который вы хотите продать");
-    int temp = Convert.ToInt32(Console.ReadLine());
-    foreach (Product t in products)
-    {
-        if (temp == t.id)
-        {
-            Console.WriteLine("напиишите колличество товара который хотите продать");
-            int temp1 = Convert.ToInt32(Console.ReadLine());
-            if (t.quantity >= temp1)
-            {
-                t.quantity -= temp1;
-                
-            }
-            flag = false;
-
-        }
-      
-    }
-    if (flag)
-    {
-        Console.WriteLine("вашего товара не нашлось в списке(");
-    }
-}
-
-static void SearchProducts(List<Product> products)
-{
-    Console.WriteLine("Введите поиск по какому признаку вы хотите осуществить\n" +
-    "id-1\n" +
-    "name-2\n" +
-    "category-3\n");
-    int temp3 = Convert.ToInt32(Console.ReadLine());
-    switch (temp3)
-    {
-        case 1:
-            poiskID();
-            break;
-        case 2:
-            poiskName();
-            break;
-        case 3:
-            poiskKategory();
-            break;
-    }
-
-    void poiskID()
-    {
-        bool flag = true;
-        Console.WriteLine("Введите id товара");
-        int temp = Convert.ToInt32(Console.ReadLine());
-        foreach (Product t in products)
-        {
-            if (t.id == temp)
-            {
-                Console.WriteLine($"{t.Name}, {t.id}, {t.price}, {t.quantity},{t.InStock},{t.Category}");
-                flag = false;
-            }
-        }
-        if (flag) { Console.WriteLine("товар не найден"); }
-    }
-    void poiskName()
-    {
-        bool flag = true;
-        Console.WriteLine("Введите навзание товара");
-        string temp = Console.ReadLine();
-        foreach (Product t in products)
-        {
-            if (t.Name == temp)
-            {
-                Console.WriteLine($"{t.Name}, {t.id}, {t.price}, {t.quantity},{t.InStock},{t.Category}");
-            }
-
-        }
-        if (flag) { Console.WriteLine("товар не найден"); }
-
-
-    }
-
-    void poiskKategory()
-    {
-        bool flag = true;
-        Console.WriteLine("выберете категорию water=1,\r\n snack=2,\r\n chebumany=3");
-        int temp = Convert.ToInt32(Console.ReadLine());
-        foreach (Product t in products)
-        {
-            if (t.Category == (Category)temp)
-            {
-                Console.WriteLine($"{t.Name}, {t.id}, {t.price}, {t.quantity},{t.InStock},{t.Category}");
-            }
-        }
-        if (flag) { Console.WriteLine("товар не найден"); }
-
-    }
-}
-
-public enum Category
-{
-    food = 0,
-    toys = 1,
-    electro = 2
-}
-
-public class Product
-{
-    static public int ids = 0;
-    public int id { get; }
-    
-    public string Name { get; set; }
-    public decimal price { get; set; }
-    public int quantity { get; set; }
-    public bool InStock => quantity > 0;
-    public Category Category { get; set; }
-
-    public Product(string n, decimal p, int q, Category category)
-    {
-        if (string.IsNullOrWhiteSpace(n)) throw new ArgumentException("Название товара не должно быть пустым");
-        if (p <= 0) throw new ArgumentException("Цена должна быть положительной");
-        if (q < 0) throw new ArgumentException("Количество не может быть отрицательным");
-        ids += 1;
-        id += ids;
-        Name = n.Trim();
-        price = p;
-        quantity = q;
-        Category = category;
-    }
-    public void Vivod() { Console.WriteLine($"ID: {id} | Название: {Name} | Цена: {price:F2} | Количество {quantity} В наличии: {(InStock ? "Да" : "Нет")} | Категория: {Category} "); }
 }
